224da58177084a677cbff66f49017ec5af084b9e
EC2 Default User <ec2-user@ip-172-31-40-154.ap-northeast-1.compute.internal>
2020-12-07 13:35:14 +0000

    bugfix

--- src/domains/App.ts
+++ src/domains/App.ts
@@ -1,7 +1,17 @@
-import { immerable } from 'immer';
+import { immerable, produce } from 'immer';
 
 export default class AppState {
   [immerable] = true;
 
   count: number = 0;
+
+  plus = (num: number) =>
+    produce(this, (draft) => {
+      draft.count += num;
+    });
+
+  minus = (num: number) =>
+    produce(this, (draft) => {
+      draft.count -= num;
+    });
 }
